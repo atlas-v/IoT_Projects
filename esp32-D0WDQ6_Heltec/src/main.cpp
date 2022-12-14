f8c62beb712d23e75f240926df774b1f47ceeeb3
atlas-v <rbaumann99@gmail.com>
2022-12-14 11:53:29 -0500

    delay

--- esp32-D0WDQ6_Heltec/src/main.cpp
+++ esp32-D0WDQ6_Heltec/src/main.cpp
@@ -24,7 +24,8 @@
 #define OLED_RESET  16
 // OLED Used
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C g_OLED(U8G2_R0, 15, 4,  16);
-// Specify line height
+
+// lineHeight
 int g_lineHeight = 0;
 
 // Replace with your network credentials (STATION)
@@ -36,20 +37,23 @@ unsigned long previousMillis = 0;
 // Timeout set == 3s
 unsigned long interval = 3000;
 
+
+//-----------------------------------------------------------------------------
 // wifi connect func
 void initWiFi()
 {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   delay(1000);
-  Serial.print("Connecting to WiFi ..");
+  Serial.print("\nConnecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println('.');
-    delay(1000);
-    WiFi.begin(ssid,    password);
+    delay(100);
+    WiFi.begin(ssid,password);
   }
   Serial.println(WiFi.localIP());  
 }
+//-----------------------------------------------------------------------------
 
 
 
@@ -69,11 +73,12 @@ void setup() {
   g_OLED.setCursor(0, g_lineHeight *  2);
   g_OLED.sendBuffer();
 
+  // Serial stream baud 9600
   Serial.begin(9600);
   initWiFi();
   Serial.print("RSSI: ");
   Serial.println(WiFi.RSSI());
-  // Display net stats
+  // Display net stats on board
   g_OLED.clear();
   g_OLED.setCursor(0, g_lineHeight);
   g_OLED.print("IP Address: ");
@@ -93,6 +98,7 @@ void loop() {
   {
     Serial.print(millis());
     Serial.println("Reconnecting to WiFi...");
+    // call the reconnect func.
     WiFi.disconnect();
     WiFi.reconnect();
     previousMillis = currentMillis;
@@ -102,7 +108,7 @@ void loop() {
         
     // always set the cursor below IP Address header
     g_OLED.setCursor(0, g_lineHeight *  3);
-    g_OLED.print((String)"Wifi: "+WiFi.RSSI()+" dB");
+    g_OLED.print((String)"Wifi Strength: "+WiFi.RSSI()+" dB");
     // Blink the on board LED
     digitalWrite(LED_BUILTIN, 1);
     g_OLED.sendBuffer();
