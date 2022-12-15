339d3865f895a11eab2947287047a68943865afc
atlas-v <rbaumann99@gmail.com>
2022-12-15 12:21:24 -0500

    0.0.2

--- esp32-D0WDQ6_Heltec/src/main.cpp
+++ esp32-D0WDQ6_Heltec/src/main.cpp
@@ -21,45 +21,44 @@
 
 // Global Vars
 
-// FIRMWARE REV
-const char *_FIRMWARE = "0.0.2";
-// RELEASE DATE
-const char *_RELEASE_DATE = "12/15/2022";
+  // FIRMWARE REV
+  const char *_FIRMWARE = "0.0.2";
+  // RELEASE DATE
+  const char *_RELEASE_DATE = "12/15/2022";
 
-// See pinout diagram for _v2
-#define OLED_CLOCK 15
-#define OLED_DATA 4
-#define OLED_RESET 16
+  // See pinout diagram for _v2
+  #define OLED_CLOCK 15
+  #define OLED_DATA 4
+  #define OLED_RESET 16
 
-// OLED Used
-/*
-Params:
-Rotation -  U8G2_R0 - none 
-            U8G2_R1 - 90
-            U8G2_R2 - 180 
-            U8G2_R3 - 270
-            U8G2_MIRROR - mirrored
-            U8G2_MIRROR_VERTICAL	- vertical mirror
-Clock Pin - 15
-Data Pin - 4
-Reset Pin - 16
-*/
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C g_OLED(U8G2_R0, 15, 4, 16);
+  // OLED Used
+  /*
+  Params:
+  Rotation -  U8G2_R0 - none 
+              U8G2_R1 - 90
+              U8G2_R2 - 180 
+              U8G2_R3 - 270
+              U8G2_MIRROR - mirrored
+              U8G2_MIRROR_VERTICAL	- vertical mirror
+  Clock Pin - 15
+  Data Pin - 4
+  Reset Pin - 16
+  */
+  U8G2_SSD1306_128X64_NONAME_F_SW_I2C g_OLED(U8G2_R0, 15, 4, 16);
 
+  // init
+  int runCounter = 0;
+  int g_lineHeight = 0;
+  // init max lines and lines
+  int maxLines = 0;
+  // max expected lines -- 30 for now
+  int line[30];
 
+  // Replace with your network credentials (STATION)
+  const char *ssid = "JRGuestWireless";
+  const char *password = "jrwelcomesu";
 
-// init
-int runCounter = 0;
-int g_lineHeight = 0;
-// init max lines and lines
-int maxLines = 0;
-// max expected lines -- 30 for now
-int line[30];
-
-// Replace with your network credentials (STATION)
-const char *ssid = "JRGuestWireless";
-const char *password = "jrwelcomesu";
-
+// Global Vars End
 
 
 
